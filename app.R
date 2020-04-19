c4379c2ef6a27193ecc235cd6be9f97e7570f720
Raymond Balise <balise@miami.edu>
2020-04-19 12:55:25 -0400

    Fix references to STATA variables

--- app.R
+++ app.R
@@ -61,7 +61,7 @@ ui <- navbarPage(
            fluidRow(
              selectInput(inputId = "state1",
                          label = "Select a State:",
-                         choices = unique(mexico$place),
+                         choices = unique(mexico$`State Name`),
                          selected = "Chiapas")
            ),
            fluidRow(
@@ -72,7 +72,7 @@ ui <- navbarPage(
            fluidRow(
              selectInput(inputId = "state2",
                          label = "Select a State:",
-                         choices = unique(mexico$place),
+                         choices = unique(mexico$`State Name`),
                          selected = "Veracruz")
            ),
            fluidRow(
@@ -95,23 +95,23 @@ ui <- navbarPage(
 )
 
 server <- function(input, output, session) {
-  place1 <- reactive({
+  State_Name1 <- reactive({
     mexico %>% 
-      filter(place == input$state1)
+      filter(`State Name` == input$state1)
   })
-  place2 <- reactive({
+  State_Name2 <- reactive({
     mexico %>% 
-      filter(place == input$state2)
+      filter(`State Name` == input$state2)
   })
   
   output$plot1 <- renderPlot({
-    ggplot(place1()) +
+    ggplot(State_Name1()) +
       geom_bar(aes(`Total Cases`)) +
       theme_minimal()
   }) 
   
   output$plot2 <- renderPlot({
-    ggplot(place2()) +
+    ggplot(State_Name2()) +
       geom_bar(aes(`Total Cases`)) +
       theme_minimal()
   }) 
@@ -119,20 +119,20 @@ server <- function(input, output, session) {
   output$refplot <- renderPlotly({
     state1_label <- input$state1
     state2_label <- input$state2
-    label1_pos <- max(place1()$`Index Adjusted for Time`, na.rm = TRUE)
-    label2_pos <- max(place2()$`Index Adjusted for Time`, na.rm = TRUE)
+    label1_pos <- max(State_Name1()$`Policy Index Adjusted for Time`, na.rm = TRUE)
+    label2_pos <- max(State_Name2()$`Policy Index Adjusted for Time`, na.rm = TRUE)
     
     x <- ggplot() + 
       #theme_few() +
-      geom_line(data = place1(), 
+      geom_line(data = State_Name1(), 
                 aes(x=`Days Since the first case (in Mexico)`, 
-                    y = `Index Adjusted for Time`, group = place), color = "orange") +
-      geom_text(data = place1(), x = 45, y = label1_pos + 5, 
+                    y = `Policy Index Adjusted for Time`, group = `State Name`), color = "orange") +
+      geom_text(data = State_Name1(), x = 45, y = label1_pos + 5, 
                 label = state1_label, color = "orange") +
-      geom_line(data = place2(), 
+      geom_line(data = State_Name2(), 
                 aes(x=`Days Since the first case (in Mexico)`, 
-                    y = `Index Adjusted for Time`, group = place), color = "gray") +
-      geom_text(data = place2(), x = 45, y = label2_pos + 5, 
+                    y = `Policy Index Adjusted for Time`, group = `State Name`), color = "gray") +
+      geom_text(data = State_Name2(), x = 45, y = label2_pos + 5, 
                 label = state2_label, color = "gray") +
       ylim (0, 45) + 
       theme_minimal()
